bfbf56c9eb4156c9092903284597e437e55b15f1
bchen41 <bettychen41@outlook.com>
2022-01-05 22:15:31 -0500

    created username and email field and function to validate email

--- models/User.js
+++ models/User.js
@@ -1,21 +1,39 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+var validateEmail = function (email) {
+  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return emailRegex.test(email);
+};
 
 const userSchema = new Schema(
   {
     // TODO: create username field
-
+    username: [
+      {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+    ],
     // TODO: create email field
-    
+    email: [
+      {
+        type: String,
+        unique: true,
+        required: true,
+        validate: [validateEmail, "Please enter a valid email address."],
+      },
+    ],
     thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Thought',
+        ref: "Thought",
       },
     ],
     friends: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'User',
+        ref: "User",
       },
     ],
   },
@@ -27,10 +45,10 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.virtual('friendCount').get(function () {
+userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
