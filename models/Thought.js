76c086d390cc3dc2b0ed0d306884b776b3c5d1e0
bchen41 <bettychen41@outlook.com>
2022-01-05 22:16:26 -0500

    created thoughtText, createdAt, username and reactions field and toJSON option

--- models/Thought.js
+++ models/Thought.js
@@ -1,27 +1,50 @@
-const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+const dateFormat = require("../utils/dateFormat");
 
 const thoughtSchema = new Schema(
   {
     // TODO: add thoughtText
-
+    thoughtText: [
+      {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+    ],
     // TODO: add createdAt
-
+    createdAt: [
+      {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    ],
     // TODO: add username
-
+    username: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
     // TODO: add reactions
-
+    reactions: [reactionSchema],
   },
   {
     // TODO: Add toJSON option
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
   }
 );
 
-thoughtSchema.virtual('reactionCount').get(function() {
+thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
 
-const Thought = model('Thought', thoughtSchema);
+const Thought = model("Thought", thoughtSchema);
 
 module.exports = Thought;
