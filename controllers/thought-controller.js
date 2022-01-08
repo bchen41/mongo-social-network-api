9b214c5b1395e433ddc426fec090de0318e33d9b
bchen41 <bettychen41@outlook.com>
2022-01-07 20:41:23 -0500

    removed comments

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,16 +1,13 @@
 const { Thought, User } = require("../models");
 
 const thoughtController = {
-  // get all thoughts
   getThoughts(req, res) {
-    // TODO: Your code here
     Thought.find()
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
-  // get single thought by id
+
   getSingleThought(req, res) {
-    // TODO: Your code
     Thought.findOne({ _id: req.params.thoughtId })
       .then((thought) =>
         !thought
@@ -19,14 +16,8 @@ const thoughtController = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  // create a thought
+
   createThought(req, res) {
-    // TODO: create a thought and add the thought to user's thoughts array
-    // findOne user with req.body.userId
-    // if (!user) return res.status(404).json
-    // else {1. create thought
-    // 2. res.json("Created the thought!")
-    // }
     Thought.create(req.body)
       .then((thought) => {
         return User.findOneAndUpdate(
@@ -47,9 +38,7 @@ const thoughtController = {
       });
   },
 
-  // update thought
   updateThought(req, res) {
-    // TODO: update thought
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
       { $set: req.body },
@@ -69,7 +58,6 @@ const thoughtController = {
       });
   },
 
-  // delete thought
   deleteThought(req, res) {
     Thought.findOneAndRemove({ _id: req.params.thoughtId })
       .then((dbThoughtData) => {
@@ -77,7 +65,6 @@ const thoughtController = {
           return res.status(404).json({ message: "No thought with this id!" });
         }
 
-        // remove thought id from user's `thoughts` field
         return User.findOneAndUpdate(
           { thoughts: req.params.thoughtId },
           { $pull: { thoughts: req.params.thoughtId } },
@@ -98,9 +85,7 @@ const thoughtController = {
       });
   },
 
-  // add a reaction to a thought
   addReaction(req, res) {
-    //  TODO: add reaction to thought's reaction array
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
       { $addToSet: { reactions: req.body } },
@@ -114,9 +99,7 @@ const thoughtController = {
       .catch((err) => res.status(500).json(err));
   },
 
-  // remove reaction from a thought
   removeReaction(req, res) {
-    // TODO: remove reaction from thoughts
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
       { $pull: { reactions: { reactionId: req.params.reactionId } } },
